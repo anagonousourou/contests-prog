8df3b3cc79a62f4270010a63025845aecde8c87a
ANAGONOU Sourou Patrick <patrick@anagonou.fr>
2026-07-26 14:04:35 +0200

    Update MaxProduct.java

--- Java/codingame/src/main/java/com/spa/leetcode/easy/MaxProduct.java
+++ Java/codingame/src/main/java/com/spa/leetcode/easy/MaxProduct.java
@@ -4,9 +4,22 @@ import java.util.Arrays;
 
 public class MaxProduct {
 
-    // TODO https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+    // https://leetcode.com/problems/maximum-product-of-three-numbers/submissions/2081988657/?envType=daily-question&envId=2026-07-26
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
+        int[] negativeNums = Arrays.stream(nums).filter(x -> x < 0).sorted().toArray();
+        int[] posNums = Arrays.stream(nums).filter(x -> x >= 0).sorted().toArray();
+
+
+        if(negativeNums.length >= 2 && posNums.length > 0) {
+            int i = negativeNums[0] * negativeNums[1] * posNums[posNums.length - 1];
+            if(posNums.length >= 3){
+                int j = posNums[posNums.length - 1] * posNums[posNums.length - 2] * posNums[posNums.length - 3];
+                return Math.max(i, j);
+            }
+
+            return i;
+        }
         return nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
     }
 }
